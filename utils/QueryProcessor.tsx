47665f145611dba4ca9638abc5566a363fe9020e
pm3512 <aobolens@andrew.cmu.edu>
2023-03-14 15:51:55 -0400

    adds fixed and includes maps

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,14 +1,25 @@
 export default function QueryProcessor(query: string): string {
   const lower = query.toLowerCase();
-  if (lower.includes('name')) {
-    return 'Alexander Obolenskiy'
-  }
-  if (lower.includes("shakespeare")) {
-    return (
+  const fixed_map = new Map([
+    [ 'what is your name?', 'Alexander Obolenskiy' ],
+  ])
+
+  const includes_map = new Map([
+    [ 'shakespeare', 
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
       "English poet, playwright, and actor, widely regarded as the greatest " +
-      "writer in the English language and the world's pre-eminent dramatist."
-    );
+      "writer in the English language and the world's pre-eminent dramatist.",
+    ], 
+  ])
+
+  if (lower in fixed_map) {
+    return fixed_map.get(lower) as string;
+  }
+
+  for (const key in includes_map) {
+    if (lower.includes(key)) {
+      return includes_map.get(key) as string;
+    }
   }
 
   return "";
