de344a697124c96b0fc90d794d05c2baa0aa9c58
pm3512 <aobolens@andrew.cmu.edu>
2023-03-14 15:54:47 -0400

    fixed maps

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -2,6 +2,13 @@ export default function QueryProcessor(query: string): string {
   const lower = query.toLowerCase();
   const fixed_map = new Map([
     [ 'what is your name?', 'Alexander Obolenskiy' ],
+    [ 'what is your name?', 'Alexander Obolenskiy' ],
+    [ 'what is your name?', 'Alexander Obolenskiy' ],
+    [ 'what is your name?', 'Alexander Obolenskiy' ],
+    [ 'what is your name?', 'Alexander Obolenskiy' ],
+    [ 'what is your name?', 'Alexander Obolenskiy' ],
+    [ 'what is your name?', 'Alexander Obolenskiy' ],
+    [ 'what is your name?', 'Alexander Obolenskiy' ],
   ])
 
   const includes_map = new Map([
@@ -10,13 +17,33 @@ export default function QueryProcessor(query: string): string {
       "English poet, playwright, and actor, widely regarded as the greatest " +
       "writer in the English language and the world's pre-eminent dramatist.",
     ], 
+    [ 'shakespeare', 
+      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+      "English poet, playwright, and actor, widely regarded as the greatest " +
+      "writer in the English language and the world's pre-eminent dramatist.",
+    ], 
+    [ 'shakespeare', 
+      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+      "English poet, playwright, and actor, widely regarded as the greatest " +
+      "writer in the English language and the world's pre-eminent dramatist.",
+    ], 
+    [ 'shakespeare', 
+      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+      "English poet, playwright, and actor, widely regarded as the greatest " +
+      "writer in the English language and the world's pre-eminent dramatist.",
+    ], 
+    [ 'shakespeare', 
+      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+      "English poet, playwright, and actor, widely regarded as the greatest " +
+      "writer in the English language and the world's pre-eminent dramatist.",
+    ], 
   ])
 
-  if (lower in fixed_map) {
+  if (fixed_map.has(lower)) {
     return fixed_map.get(lower) as string;
   }
 
-  for (const key in includes_map) {
+  for (const key in includes_map.keys()) {
     if (lower.includes(key)) {
       return includes_map.get(key) as string;
     }
