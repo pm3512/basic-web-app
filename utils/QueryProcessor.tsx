a4a0056c50ec5a125533ecadce9442537d711d0d
pm3512 <aobolens@andrew.cmu.edu>
2023-03-14 15:41:02 -0400

    name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,5 +1,9 @@
 export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes("shakespeare")) {
+  const lower = query.toLowerCase();
+  if (lower.includes('name')) {
+    return 'Alex'
+  }
+  if (lower.includes("shakespeare")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
       "English poet, playwright, and actor, widely regarded as the greatest " +
