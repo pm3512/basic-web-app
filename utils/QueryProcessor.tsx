aeae2b69ab6b4d30c8bcd2d261c0c5d2bc57d0ae
pm3512 <aobolens@andrew.cmu.edu>
2023-03-14 16:20:36 -0400

    added mul

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -34,6 +34,13 @@ export default function QueryProcessor(query: string): string {
     return sum.toString();
   }
 
+  if(lower.startsWith('what is ') && lower.includes(' minus ')) {
+    const suff = lower.slice('what is '.length).replaceAll('?', '');
+    const nums = suff.split(' minus ');
+    const sum = nums.map((x) => parseInt(x)).reduce((a, b) => a - b);
+    return sum.toString();
+  }
+
   if(lower.startsWith('what is ') && lower.includes(' multiplied by ')) {
     const suff = lower.slice('what is '.length).replaceAll('?', '');
     const nums = suff.split(' multiplied by ');
